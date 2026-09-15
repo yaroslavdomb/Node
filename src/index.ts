709cc1929b8aa2332169c880eb276ad196a805ef
Yaroslav Domb <yaroslav.domb@gmail.com>
2026-09-15 12:00:02 +0300

    Adding docs for final task

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
 import validatedEnv from "./config/env.config";
 import express from "express";
 import path from "node:path";
-import morgan from "morgan";
+import cors from "cors";
+//import morgan from "morgan";
 import usersRouter from "./routers/users";
 import cardsRouter from "./routers/cards";
-import loggerChalk from "./middleware/logger-chalk";
+//import loggerChalk from "./middleware/logger-chalk";
 import loggerPC from "./middleware/logger-pc";
 import notFound from "./middleware/notFound";
 import dbConnection from "./db/connection";
 import errorHandler from "./middleware/error-handler";
+import { logger } from "./logs/logger";
+import pinoHttp from "pino-http";
 
 dbConnection();
 
-const app = express();
-
-console.log("Starting application ...");
-
 //Initial configurations for server
+const app = express();
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(pinoHttp({ logger }));
+//app.use(morgan("dev"));
 app.use(loggerPC);
+app.use(
+  cors({
+    origin: ["http://localhost:5137"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    credentials: true
+  })
+);
+
+//console.log("Starting application ...");
+logger.info("Starting application ...");
 
 //Paths for routers
 app.use("/static", express.static(path.resolve("public")));
